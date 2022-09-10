e6741a280c5196361a8bd9140f2422a8011d6dbe
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-09-11 01:14:24 +0800

    fix: delete all unnecessary files and install some libraries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,68 @@
-import * as React from 'react';
+import { client, urlFor } from 'lib/client';
+import { BannerTypes, ProductTypes } from 'lib/dataTypes';
+import React from 'react';
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
+import { FooterBanner, HeroBanner, Product } from '@/components';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
-export default function HomePage() {
-  return (
-    <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
-      <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
-          </div>
-        </section>
-      </main>
-    </Layout>
-  );
+interface HomeProps {
+  productsData: ProductTypes[];
+  bannerData: BannerTypes[];
 }
+
+const Home = ({ productsData, bannerData }: HomeProps) => {
+  // console.log('product', productsData);
+  // console.log(typeof productsData);
+  // console.log('banner', bannerData);
+  // console.log(typeof bannerData);
+  // console.log('banner');
+  // console.log(urlFor(bannerData[0].image));
+  // console.log('product');
+  // console.log(urlFor(productsData[0].image));
+  return (
+    <>
+      <HeroBanner
+        largeText={bannerData[0]?.largeText}
+        product={bannerData[0].product}
+        description={bannerData[0].desc}
+        discount={bannerData[0].discount}
+        image={urlFor(bannerData[0].image).url()}
+        buttonText={bannerData[0].buttonText}
+      />
+      <div className='mt-4 flex text-center'>
+        <div className='container mx-auto'>
+          <h2 className='text-4xl font-extrabold text-gray-900'>
+            Best Seller Products
+          </h2>
+          <p>speaker There are many variations passages</p>
+        </div>
+      </div>
+
+      <div className='container mx-auto mt-4 grid grid-cols-4 gap-4'>
+        {productsData?.map((product) => (
+          <Product
+            key={product._id}
+            name={product.name}
+            price={product.price}
+            slug={product.slug}
+            image={urlFor(product.image && product.image[0]).url()}
+          />
+        ))}
+      </div>
+
+      <FooterBanner />
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const productQuery = '*[_type == "product"]';
+  const productsData = await client.fetch(productQuery);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { productsData, bannerData },
+  };
+}
+
+export default Home;
